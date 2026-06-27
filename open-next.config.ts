4741c3820c14b952d182866c0df541f6a0be5e26
troubling-1 <865111370@qq.com>
2026-06-27 14:40:39 +0800

    更新说明

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig();
+const cloudflareConfig = defineCloudflareConfig();
+
+export default {
+  ...cloudflareConfig,
+  buildCommand: "npx --no-install next build",
+};
